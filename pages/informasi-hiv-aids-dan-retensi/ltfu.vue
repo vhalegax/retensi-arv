2a1b2364f0eb37fa35a5ad77bc55bb6e3db65dd8
Wisnu Setyawan <stephanus.ws@gmail.com>
2021-11-29 08:45:47 +0700

    update content

--- pages/informasi-hiv-aids-dan-retensi/ltfu.vue
+++ pages/informasi-hiv-aids-dan-retensi/ltfu.vue
@@ -1,5 +1,14 @@
 <template>
   <v-row>
-    <v-col> Putus ARV </v-col>
+    <v-col>
+      <b>LTFU adalah :</b>
+      <ol class="mt-3">
+        <li>
+          Kejadian drop out/LTFU ditemukan karena efek samping obat, infeksi
+          oportunistik, kesulitan dalam mematuhi jadwal mengkonsumsi ARV faktor
+          usia 18-35 tahun dan status pekerjaan
+        </li>
+      </ol>
+    </v-col>
   </v-row>
 </template>
